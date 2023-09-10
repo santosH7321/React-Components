0069102f164065abd2d8a91a1900bd31f4f09fa8
Santosh <santoshkumar23kky@gmail.com>
2023-09-10 22:46:42 +0530

    Components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Item from "./Components/Item";
+
+function App() {
+  return (
+    <>
+    <Item/>
+     <h1 className="text-3xl font-semibold bg-gray-900 text-white p-3 flex  justify-center">Hello Santosh ab Ract With tailwindcss</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Item.js
@@ -0,0 +1,11 @@
+function Item (){
+    return (
+        <div>
+             <p className="text-xl bg-sky-300 flex justify-center items-center p-4 ">Nishant Ranjan</p>
+             <hr/>
+             <p className="text-xl bg-sky-300 flex justify-center items-center p-4 ">Hello Vivek</p>
+        </div>
+       
+    );
+}
+export default Item;
